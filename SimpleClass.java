af42b69c7c54a9b9edd4e0364ec6b114060124c5
GundapuJaahnavi <jaahnavigundapu@gmail.com>
2023-06-03 13:08:48 -0500

    Even or odd

--- SimpleClass.java
+++ SimpleClass.java
@@ -1,6 +1,17 @@
+import java.util.*;
+
 public class SimpleClass {
     public static void main(String[] args){
         System.out.println("Hello world!!");
+        Scanner c = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        Integer number= c.nextInt();
+        if(number % 2 == 0){
+            System.out.println(number + " Is an even digit");
+        }
+        else {
+            System.out.println(number + " Is an odd digit");
+        }
 
     }
 }
